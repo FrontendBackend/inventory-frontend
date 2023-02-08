1eb36806024f9a49f17f3da6efc9fbf7cff17b42
juanvaleriomayta <juanvalerio785@gmail.com>
2023-02-07 21:32:06 -0500

    Nuevos cambios

--- src/app/modules/dashboard/components/home/home.component.ts
+++ src/app/modules/dashboard/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  data: string[][] | any;
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
   }
+
+   upload(event: any) {
+      let file = event.target.files[0];
+      let formData = new FormData();
+      formData.append('file', file);
+
+      this.http.post('http://localhost:1999/inv/api/v1/import-excel', formData)
+         .subscribe( (data: string[][] | any) => {
+          this.data = data;
+       });
+   }
 
 }
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './pages/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -15,7 +16,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     RouterModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ]
 })
 export class DashboardModule { }
