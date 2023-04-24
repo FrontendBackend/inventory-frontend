4888439dc4658f6ebe5412da8080d445ea0c0a84
juanvaleriomayta <juanvalerio785@gmail.com>
2023-04-24 00:49:16 -0500

    Avance de las operación del prestamo historial

--- src/app/modules/dashboard/prestamo/prestamo/prestamo-general/prestamo-general.component.ts
+++ src/app/modules/dashboard/prestamo/prestamo/prestamo-general/prestamo-general.component.ts
@@ -10,6 +10,7 @@ import { TblPrestamoDTO } from 'src/app/modules/shared/models/TblPrestamoDTO';
 import { ETipoAccionCRUD } from 'src/app/modules/shared/models/tipo-accion';
 import { PersonaService } from 'src/app/modules/shared/services/persona.service';
 import { PrestamoHistorialListaComponent } from '../prestamo-historial-lista/prestamo-historial-lista.component';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-prestamo-general',
@@ -28,12 +29,20 @@ export class PrestamoGeneralComponent implements OnInit {
   esFormularioSoloLectura = false;
   frmReactivoPrestamo: FormGroup;
   enProcesoPrestamo = false
+  habilitarMontoPositivo = false
+  habilitarLecturaMonto = false
+  habilitarLecturaMontoAOperar = false
   total: number;
+
+  /* Seria resta cuando es "false" */
+  flOperador = false;
+  flOperadorCadena = '0';
+
   constructor(private formBuilder: FormBuilder, private personaService: PersonaService,
     private matSnackBar: MatSnackBar,) { }
 
   ngOnInit(): void {
-    this.accion = (this.tipoAccionCrud === ETipoAccionCRUD.MODIFICAR) ? 'MODIFICAR': '';
+    this.accion = (this.tipoAccionCrud === ETipoAccionCRUD.MODIFICAR) ? 'MODIFICAR' : '';
 
     this.iniciarFormularioPrestamo();
   }
@@ -64,6 +73,7 @@ export class PrestamoGeneralComponent implements OnInit {
         descripcion: [this.tblPrestamoDTO.descripcion, [Validators.required]],
         monto: [this.tblPrestamoDTO.monto, [Validators.required]],
         montoOperar: [''],
+        descripcionHistorial: ['', [Validators.required]],
       }
     );
 
@@ -79,9 +89,37 @@ export class PrestamoGeneralComponent implements OnInit {
   calcularMontoSubTotal(): void {
     const valorMontoOperar = this.frmReactivoPrestamo.value.montoOperar;
 
-    const total =  Number(this.tblPrestamoDTO.monto) - Number(valorMontoOperar);
+    let total = null;
+
+    if (this.flOperador === true) {
+      total = Number(this.tblPrestamoDTO.monto) + Number(valorMontoOperar);
+
+    } else if (this.flOperador === false) {
+      total = Number(this.tblPrestamoDTO.monto) - Number(valorMontoOperar);
+    }
 
     this.total = total;
+
+    /* El total menor que 0, se deshabilita el botón de modificar y se habilita caja 'monto' para no ser ingresado el valor */
+    if (total < 0) {
+      this.habilitarMontoPositivo = true;
+      this.habilitarLecturaMonto = true;
+    }
+    /* El total menor que 0, se habilita el botón de modificar y se habilita caja 'monto' para no ser ingresado el valor */
+    else if (total === 0) {
+      this.habilitarMontoPositivo = false;
+      this.habilitarLecturaMonto = true;
+    }
+    /* El total mayor que 0, se habilita el botón de modificar y se habilita caja 'monto' para no ser ingresado el valor. */
+    else if (total > 0) {
+      this.habilitarMontoPositivo = false;
+      this.habilitarLecturaMonto = true;
+
+      /* Dado que la caja 'monto a restar o sumar' sea = '0' se habilita la caja 'monto' */
+      if (valorMontoOperar === 0 || valorMontoOperar === '') {
+        this.habilitarLecturaMonto = false;
+      }
+    }
 
     this.frmReactivoPrestamo.controls['monto'].setValue(total);
   }
@@ -103,9 +141,9 @@ export class PrestamoGeneralComponent implements OnInit {
     tblPrestamoDTOCU.fePrestamo = this.frmReactivoPrestamo.controls['fePrestamo'].value;
     tblPrestamoDTOCU.feCancelado = this.frmReactivoPrestamo.controls['feCancelado'].value;
 
-    if(tblPrestamoDTOCU.monto === 0){
+    if (tblPrestamoDTOCU.monto === 0) {
       tblPrestamoDTOCU.estado = '1'; // Marca como estado 'cancelado'
-    }else{
+    } else {
       tblPrestamoDTOCU.estado = '0'; // Marca como estado 'pendiente'
     }
     // Recuperando las propiedades originales
@@ -125,13 +163,17 @@ export class PrestamoGeneralComponent implements OnInit {
           duration: 4000
         });
 
-        if(this.frmReactivoPrestamo.value.montoOperar > 0){
+        if (this.frmReactivoPrestamo.value.montoOperar > 0) {
           let tblHistorialPrestamoDTOCU = new TblHistorialPrestamoDTO()
           tblHistorialPrestamoDTOCU.idPrestamo = respuesta.idPrestamo;
           tblHistorialPrestamoDTOCU.montoSubtotalOperado = this.tblPrestamoDTO.monto;
+          tblHistorialPrestamoDTOCU.descripcionHistorial = this.frmReactivoPrestamo.value.descripcionHistorial;
+          tblHistorialPrestamoDTOCU.flagOperador = this.flOperadorCadena;
           tblHistorialPrestamoDTOCU.montoOperar = this.frmReactivoPrestamo.value.montoOperar;
           this.tblPrestamoDTO = respuesta;
           this.procesarCrearHistorialPrestamo(tblHistorialPrestamoDTOCU)
+
+          this.frmReactivoPrestamo.controls['montoOperar'].setValue(0);
         }
 
         this.enProcesoPrestamo = false;
@@ -144,15 +186,17 @@ export class PrestamoGeneralComponent implements OnInit {
 
     this.personaService.crearHistorialPrestamo(tblHistorialPrestamoDTO).subscribe(respuesta => {
 
-      console.log({procesarCrearHistorialPrestamo: respuesta});
+      // console.log({ procesarCrearHistorialPrestamo: respuesta });
 
       if (respuesta.tipoResultado === TipoResultado.SUCCESS.toString()) {
+
         this.matSnackBar.open(respuesta.mensaje, 'Ok', {
           duration: 4000
         });
         this.prestamoHistorialListaComponent.paginarHistorialPrestamos();
         this.enProcesoPrestamo = false;
-      } else if (respuesta.tipoResultado === TipoResultado.ERROR.toString()){
+
+      } else if (respuesta.tipoResultado === TipoResultado.ERROR.toString()) {
         this.matSnackBar.open('Ocurrió un error al intentar crear el prestamo', 'ERROR', {
           duration: 4000
         });
@@ -160,4 +204,23 @@ export class PrestamoGeneralComponent implements OnInit {
     });
   }
 
+  cambiarOperadorSumarOResta(event: MatSlideToggleChange) {
+
+    console.log({ eventoSumaresta: event });
+    if (event.checked) {
+      this.flOperador = true;
+      this.flOperadorCadena = '1'; //Modo suma
+      this.matSnackBar.open('Cambió a tipo operador sumar', 'Ok', {
+        duration: 4000
+      });
+      this.calcularMontoSubTotal();
+    } else {
+      this.flOperador = false;
+      this.flOperadorCadena = '0'; //Modo resta
+      this.matSnackBar.open('Cambió a tipo operador restar', 'Ok', {
+        duration: 4000
+      });
+      this.calcularMontoSubTotal();
+    }
+  }
 }
