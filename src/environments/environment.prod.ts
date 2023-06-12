7ccc548093e4a053218d6010b5b43aefafeb0a2a
juanvaleriomayta <juanvalerio785@gmail.com>
2023-06-11 23:39:22 -0500

    dockerizar el frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,18 @@
 export const environment = {
   production: true,
-  base_url: "http://localhost:11999/inv/api/v1",
-  HOST: "http://localhost:11999/inv/api"
+  /*
+  * ----------------------------------------------------
+  * ------- Descomentar cuando se utiliza Docker - local -------
+  * ----------------------------------------------------
+  */
+  // base_url: "http://localhost:11999/inv/api/v1",
+  // HOST: "http://localhost:11999/inv/api"
+
+  /*
+  * ----------------------------------------------------
+  * ------- Descomentar cuando se utiliza Docker - cloud -------
+  * ----------------------------------------------------
+  */
+  base_url: "http://35.211.62.133:1999/inv/api/v1",
+  HOST: "http://35.211.62.133:1999/inv/api"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,11 +9,19 @@ export const environment = {
 
   /*
   * ----------------------------------------------------
-  * ------- Descomentar cuando se utiliza Docker -------
+  * ------- Descomentar cuando se utiliza Docker - local -------
   * ----------------------------------------------------
   */
-  base_url: "http://localhost:11999/inv/api/v1",
-  HOST: "http://localhost:11999/inv/api"
+  // base_url: "http://localhost:11999/inv/api/v1",
+  // HOST: "http://localhost:11999/inv/api"
+
+  /*
+  * ----------------------------------------------------
+  * ------- Descomentar cuando se utiliza Docker - cloud -------
+  * ----------------------------------------------------
+  */
+  base_url: "http://35.211.62.133:1999/inv/api/v1",
+  HOST: "http://35.211.62.133:1999/inv/api"
 
 };
 
